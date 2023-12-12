1b7aff546304e5fd23bfd4226bb4a465b487f2ae
LuckyDancel <106247494+ChinnoDancel16@users.noreply.github.com>
2023-12-12 09:16:22 +0800

    wkwkwwkw

---
+++ js/game.js
@@ -0,0 +1,247 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the player name from localStorage (you can store it there after user input)
+    const playerName = localStorage.getItem("playerName");
+
+    // Update the text on the user side with the player name
+    const userNameText = document.getElementById("user-name-text");
+    userNameText.textContent = playerName || "User Side"; // Default text if playerName is not available
+
+    const playBoard = document.querySelector(".play-board");
+    const scoreElement = document.querySelector(".score");
+    const highScoreElement = document.querySelector(".high-score");
+    const controls = document.querySelectorAll(".controls i");
+
+    let gameOver = false;
+    let foodX, foodY;
+    let snakeX = 5, snakeY = 5;
+    let velocityX = 0, velocityY = 0;
+    let snakeBody = [];
+    let setIntervalId;
+    let score = 0;
+
+    // Getting high score from the local storage
+    let highScore = localStorage.getItem("high-score") || 0;
+    highScoreElement.innerText = `High Score: ${highScore}`;
+
+    const updateFoodPosition = () => {
+        // Passing a random 1 - 30 value as food position
+        foodX = Math.floor(Math.random() * 35) + 1;
+        foodY = Math.floor(Math.random() * 35) + 1;
+    }
+
+    const handleGameOver = () => {
+        // Clearing the timer and reloading the page on game over
+        clearInterval(setIntervalId);
+        alert("Game Over! Press OK to replay...");
+        location.reload();
+    }
+
+    const changeDirection = e => {
+        // Changing velocity value based on key press
+        if (e.key === "ArrowUp" && velocityY != 1) {
+            velocityX = 0;
+            velocityY = -1;
+        } else if (e.key === "ArrowDown" && velocityY != -1) {
+            velocityX = 0;
+            velocityY = 1;
+        } else if (e.key === "ArrowLeft" && velocityX != 1) {
+            velocityX = -1;
+            velocityY = 0;
+        } else if (e.key === "ArrowRight" && velocityX != -1) {
+            velocityX = 1;
+            velocityY = 0;
+        }
+    }
+
+    // Calling changeDirection on each key click and passing key dataset value as an object
+    controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+    const initGame = () => {
+        if (gameOver) return handleGameOver();
+        let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+        // Checking if the snake hit the food
+        if (snakeX === foodX && snakeY === foodY) {
+            updateFoodPosition();
+            snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+            score++; // increment score by 1
+            highScore = score >= highScore ? score : highScore;
+            localStorage.setItem("high-score", highScore);
+            scoreElement.innerText = `Score: ${score}`;
+            highScoreElement.innerText = `High Score: ${highScore}`;
+        }
+        // Updating the snake's head position based on the current velocity
+        snakeX += velocityX;
+        snakeY += velocityY;
+
+        // Shifting forward the values of the elements in the snake body by one
+        for (let i = snakeBody.length - 1; i > 0; i--) {
+            snakeBody[i] = snakeBody[i - 1];
+        }
+        snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+
+        // Checking if the snake's head is out of wall, if so setting gameOver to true
+        if (snakeX <= 0 || snakeX > 35 || snakeY <= 0 || snakeY > 35) {
+            return gameOver = true;
+        }
+
+        for (let i = 0; i < snakeBody.length; i++) {
+            // Adding a div for each part of the snake's body
+            html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+            // Checking if the snake head hit the body, if so set gameOver to true
+            if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+                gameOver = true;
+            }
+        }
+        playBoard.innerHTML = html;
+    }
+
+    updateFoodPosition();
+    setIntervalId = setInterval(initGame, 100);
+    document.addEventListener("keyup", changeDirection);
+});
+
+/** AI SNAKE CODE */
+
+const playBoard = document.getElementById("play-board-ai");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+
+// Getting high score from the local storage
+let highScore = parseInt(localStorage.getItem("high-score")) || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+
+const updateFoodPosition = () => {
+    // Passing a random 1 - 30 value as food position
+    foodX = Math.floor(Math.random() * 35) + 1;
+    foodY = Math.floor(Math.random() * 35) + 1;
+}
+
+const handleGameOver = () => {
+    // Clearing the timer and reloading the page on game over
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay...");
+    location.reload();
+}
+
+const changeDirection = e => {
+    // Changing velocity value based on key press
+    if (e.key === "ArrowUp" && velocityY !== 1) {
+        velocityX = 0;
+        velocityY = -1;
+        aiMove(); // Update AI movement when user changes direction
+    } else if (e.key === "ArrowDown" && velocityY !== -1) {
+        velocityX = 0;
+        velocityY = 1;
+        aiMove(); // Update AI movement when user changes direction
+    } else if (e.key === "ArrowLeft" && velocityX !== 1) {
+        velocityX = -1;
+        velocityY = 0;
+        aiMove(); // Update AI movement when user changes direction
+    } else if (e.key === "ArrowRight" && velocityX !== -1) {
+        velocityX = 1;
+        velocityY = 0;
+        aiMove(); // Update AI movement when user changes direction
+    }
+}
+
+// Calling changeDirection on each key click and passing key dataset value as an object
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+const initGame = () => {
+    if (gameOver) return handleGameOver();
+
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    // Checking if the snake hit the food
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]);
+        score++;
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
+    }
+
+    // Updating the snake's head position based on the current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    // Shifting forward the values of the elements in the snake body by one
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    snakeBody[0] = [snakeX, snakeY];
+
+    // Checking if the snake's head is out of wall, if so setting gameOver to true
+    if (snakeX <= 0 || snakeX > 35 || snakeY <= 0 || snakeY > 35) {
+        return gameOver = true;
+    }
+
+    // AI-controlled snake movement
+    aiMove();
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        // Adding a div for each part of the snake's body
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        // Checking if the snake head hit the body, if so set gameOver to true
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = html;
+}
+
+// AI-controlled snake movement function
+const aiMove = () => {
+    // Implement your AI algorithm here
+    // For simplicity, you can use a basic algorithm like moving towards the food
+    if (foodX > snakeX) {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (foodX < snakeX) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (foodY > snakeY) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (foodY < snakeY) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+}
+setIntervalId = setInterval(initGame, 100);
+
+updateFoodPosition();
+scoreElement.innerText = `Score: ${score}`;
+highScoreElement.innerText = `High Score: ${highScore}`;
+setIntervalId = setInterval(initGame, 100);
+
+document.addEventListener("keyup", changeDirection);
+
+// Function to change the player's name
+const changeName = () => {
+    const newName = prompt("Enter your name:");
+    if (newName) {
+        playerName = newName;
+        localStorage.setItem("playerName", playerName);
+        userNameText.textContent = playerName;
+    }
+}
+
+// Attach the changeName function to a button or event
+const changeNameButton = document.getElementById("change-name-button");
+changeNameButton.addEventListener("click", changeName);
+
+
+
---
+++ js/options.js
@@ -0,0 +1,33 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const startGameButton = document.getElementById("start-game-button");
+
+    startGameButton.addEventListener("click", () => {
+        const playerName = document.getElementById("player-name").value;
+        localStorage.setItem("playerName", playerName);
+        const aiDifficulty = document.getElementById("ai-difficulty").value;
+        localStorage.setItem("ai-difficulty", aiDifficulty);
+
+        if (playerName.trim() === "" || aiDifficulty === "") {
+            alert("Please enter a player name and select a difficulty.");
+            return;
+        }
+
+        // Make an asynchronous request to save_player.php
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "save_player.php", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText); // Log the server response
+                // Redirect to the game page with the selected AI difficulty
+                window.location.href = `game-page.html?difficulty=${aiDifficulty}`;
+            }
+        };
+
+        // Send data to the server
+        xhr.send(`playerName=${playerName}&aiDifficulty=${aiDifficulty}`);
+
+        window.location.href = `game-page.html?difficulty=${aiDifficulty}`;
+    });
+});
